c6d7711fc98d305512c2d1908222dcf3cb8b05e9
REDMOND\weiywang <weiywang@microsoft.com>
2017-01-17 22:23:01 -0800

    #14

---
+++ 14.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0:
+            return ''
+        if len(strs) == 1:
+            return strs[0]
+        
+        prefix = ''
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return prefix
+            prefix += strs[0][i]
+
+        return prefix
